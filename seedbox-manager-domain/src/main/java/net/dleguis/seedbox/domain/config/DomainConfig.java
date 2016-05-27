9e455ce9aa2d9f08a4f82b3138ef16e19db4d13f
Kawada <dleguis@outlook.com>
2016-05-27 18:00:04 +0200

    added config files

---
+++ seedbox-manager-domain/src/main/java/net/dleguis/seedbox/domain/config/DomainConfig.java
@@ -0,0 +1,7 @@
+package net.dleguis.seedbox.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainConfig {
+}
--- seedbox-manager-presentation/src/main/java/net/dleguis/seedbox/presentation/SeedboxManagerApplication.java
+++ seedbox-manager-presentation/src/main/java/net/dleguis/seedbox/presentation/SeedboxManagerApplication.java
@@ -1,4 +1,4 @@
-package net.dleguis;
+package net.dleguis.seedbox.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
