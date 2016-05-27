7b657f2ef024103656698be882247ba564643999
Kawada <dleguis@outlook.com>
2016-05-27 16:32:15 +0200

    Added dependencies renamed modules

---
+++ seedbox-manager-presentation/src/main/java/net/dleguis/SeedboxManagerApplication.java
@@ -0,0 +1,14 @@
+package net.dleguis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication(scanBasePackages = { "net.dleguis" })
+@EnableWebMvc
+public class SeedboxManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedboxManagerApplication.class, args);
+	}
+}
